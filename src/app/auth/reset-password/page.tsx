2da8127551d36d91cca418a07c039cd378048687
Dani Sofyan <danixsofyan@gmail.com>
2025-09-23 02:33:09 +0700

    Add placeholder pages for auth and orders
    Created basic placeholder pages for Forgot Password, Register, Reset Password, B2B Orders, and B2C Orders. These pages include simple headings and messages indicating that forms will be built later.

---
+++ src/app/auth/reset-password/page.tsx
@@ -0,0 +1,10 @@
+export default function ResetasswordPage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen p-6">
+        <h1 className="text-2xl font-bold">Reset Password</h1>
+        <p className="text-gray-600 mt-2">
+            This is a placeholder page. You can build the form later.
+        </p>
+        </div>
+    )
+}
---
+++ src/app/orders/b2c/page.tsx
@@ -0,0 +1,10 @@
+export default function b2cPage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen p-6">
+        <h1 className="text-2xl font-bold">B2C</h1>
+        <p className="text-gray-600 mt-2">
+            This is a placeholder page. You can build the form later.
+        </p>
+        </div>
+    )
+}
