1edfcb5a673142f18c6c566afabfa6718cc8887e
Dani Sofyan <danixsofyan@gmail.com>
2025-09-23 09:04:12 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${plusJakartaSans.className} bg-slate-50 dark:bg-black`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           {children}
           <div className="fixed bottom-4 right-4 z-50">
             <ModeToggle />
